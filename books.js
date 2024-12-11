660da3ad519d35487d30de1a364e463392bcf533
Nancy-Wainaina <wainainanancywambui@gmail.com>
2024-12-11 15:58:28 +0300

    made changes

--- books.js
+++ books.js
@@ -1,5 +1,11 @@
-var list2 = document.getElementById("list2")
-var scrollLeft = document.getElementById("scroll-left")
-var scrollRight = document.getElementById("scroll-right")
-scrollRight.addEventListener("click", (event)=>{list2.scrollBy(800,0);});
-scrollLeft.addEventListener("click", (event)=>{list2.scrollBy(800,0)});
+var list2 = document.getElementById("list2");
+var scrollLeft = document.getElementById("scroll-left");
+var scrollRight = document.getElementById("scroll-right");
+
+scrollRight.addEventListener("click", (event) => {
+    list2.scrollBy(800, 0); // Scroll 800px to the right
+});
+
+scrollLeft.addEventListener("click", (event) => {
+    list2.scrollBy(-800, 0); // Scroll 800px to the left
+});
